541c4f897becb1f13b6f308452e0f540b86ac302
manabu <tsutsui4012@gmail.com>
2024-03-06 02:15:13 +0900

    commit2

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -2,14 +2,14 @@ import os
 
 from .settings_common import *
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
-STATIC_ROOT = '/usr/share/nginx/html/static/'
-MEDIA_ROOT = '/usr/share/nginx/html/media/'
+STATIC_ROOT = '/usr/share/nginx/html/static'
+MEDIA_ROOT = '/usr/share/nginx/html/media'
 
 AWS_SES_ACCESS_KEY_ID = os.environ.get('AWS_SES_ACCESS_KEY_ID')
 AWS_SES_SECRET_ACCESS_KEY = os.environ.get('AWS_SES_SECRET_ACCESS_KEY')
